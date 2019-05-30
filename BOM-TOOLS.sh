79079b2617f1eff3b4f1e699992296760890f388
I.C.I <51185989+PR0CYB3R01@users.noreply.github.com>
2019-05-30 11:41:51 +0700

    Add files via upload

---
+++ BOM-TOOLS.sh
@@ -0,0 +1,99 @@
+#!/system/xbin/bash
+#gunakan tools ini untuk menginstall beberapa command termux:v
+#jangan bnyak tanya buriq:v
+
+
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+echo $green  "SELAMAT DATANG"
+sleep 1
+clear
+echo $blue  "SELAMAT DARANG"
+sleep 1
+clear
+echo $yellow  "SELAMAT DATANG"
+sleep 1
+clear
+echo $red  "SELAMAT DATANG"
+sleep 1
+clear
+echo $purple  "SELAMAT DATANG"
+sleep 1
+clear
+echo $cyan  "SELAMAT DATANG"
+sleep 1
+clear
+
+echo "\033[1;31m]
+
+•<.......................................>•
+•<jenis   :menginstall command termux    >•
+•<coded by:FIQRA ID                      >•
+•<team.   :C.W.T (CYBER WARRIOR TEAM)    >•
+•<github  :https://github.com/FIQRAID    >•
+•<kontak  :+6282283353474                >•
+•<.......................................>•
+
+echo $cyan figlet -f slant "MR.pdf"
+
+echo $cyan "1.INSTALL LANGSUNG COMMAND TERMUX"
+echo $purple "2.exit"
+read -p ">>" stc
+
+
+if [ $stc= 1 ]
+then
+clear
+echo $cyan "TUNGGU BEBERAPA MENIT"
+pkg update && pkg updrade
+pkg install cowsay
+pkg install termux-api
+pkg install clang
+pkg install tor
+pkg install sh
+pkg install python
+pkg install python2
+pkg install git
+pkg install nano
+pkg install php
+pkg install figlet
+pkg install toilet
+pkg install ruby
+pkg install bc
+pkg install vim
+pkg install wget
+pkg install curl
+pkg install bash
+pkg install gnupg
+pip2 install requests
+pip2 install requirements.txt
+pip2 install mechanize
+pip2 install urllib3 chardet certifi idna request
+pip2 install progress
+echo $cyan figlet "SELESAI"
+fi
+
+if [ $stc = 2 ]
+then
+clear
+echo $cyan figlet -f slant "EXIT"|lolcat
+sleep 2
+echo $red "TOOLS INI DIRANCANG UNTUK PENGGUNA BARU TERMUX AGAR LANCAR MENJALANKAN TERMUXNYA"
+sleep 2
+echo $yellow "TERIMA KASIH TELAH BERKUNJUNG"
+sleep 2
+echo $blue "JIKA ADA KESALAHAN TOLONG LAPORKAN KE SAYA"
+sleep 2
+echo $purple "INI NO WA SYA +6282283353474"
+sleep 2
+echo $red "sampai jumpa"
+exit
+fi
+
+
